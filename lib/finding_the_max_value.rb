29875a095b97fe785e8c23b7148e14741f1ed50a
Kevin Glasgow <kglas001@gmail.com>
2020-09-07 20:07:00 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,7 +2,7 @@ def find_max_value(array)
   count = 0 
   max_val = 0
   while count < array.length do 
-    if array.count == max_val 
+    if max_val == max_val 
       max_val = count 
     end
     count += 1 
